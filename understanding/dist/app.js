31d3c7217da58de6f8fac6fc366bb5ec88f5783d
Hemant Kumar Singh <hemant.kumar@moonraft.com>
2024-01-29 09:57:26 +0530

    Class concept getting added

--- understanding/dist/app.js
+++ understanding/dist/app.js
@@ -1,27 +1,25 @@
 "use strict";
-const button = document.querySelector("button");
-let logged;
-button === null || button === void 0 ? void 0 : button.addEventListener("click", () => {
-    console.log("Clicked");
-    logged = true;
-});
-let local = "hemant";
-function clickHandler(message) {
-    console.log("Clock" + message);
-}
-button === null || button === void 0 ? void 0 : button.addEventListener("click", clickHandler.bind(null, "me"));
-const hobbies = ["Sport", "Cooking"];
-const activeHobby = ["Hiking", ...hobbies];
-const person = {
-    name: "Max",
-    age: 30,
-};
-const copiesPerson = Object.assign({ address: "dublin", blood_group: "+0ve" }, person);
-console.log(copiesPerson);
-const add = (...numbers) => {
-    return numbers.reduce((currResult, currValue) => {
-        return currResult + currValue;
-    }, 0);
-};
-const addNum = add(5, 6, 7);
-console.log(addNum);
+var Department = (function () {
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    Department.prototype.describe = function () {
+        console.log("Department: (".concat(this.id, "): ").concat(this.name));
+    };
+    Department.prototype.addEmployee = function (employees) {
+        this.employees.push(employees);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    return Department;
+}());
+var acconting = new Department("1", "Accounting");
+acconting.describe();
+acconting.addEmployee("Max");
+acconting.addEmployee("Manu");
+acconting.addEmployee("Manaswi");
+acconting.printEmployeeInformation();
--- understanding/src/app.ts
+++ understanding/src/app.ts
@@ -1,44 +1,104 @@
-// some comment
-const button = document.querySelector("button");
+// // some comment
+// const button = document.querySelector("button");
 
-// comment
-let logged;
-button?.addEventListener("click", () => {
-  console.log("Clicked");
-  logged = true;
-});
-let local = "hemant";
-function clickHandler(message: string) {
-  console.log("Clock" + message);
+// // comment
+// let logged;
+// button?.addEventListener("click", () => {
+//   console.log("Clicked");
+//   logged = true;
+// });
+// let local = "hemant";
+// function clickHandler(message: string) {
+//   console.log("Clock" + message);
+// }
+// button?.addEventListener("click", clickHandler.bind(null, "me"));
+
+// // next gen javascript
+
+// // const add = (a: number, b: number = 2) => a + b;
+
+// // console.log(add(1));
+
+// // spread operator
+
+// const hobbies = ["Sport", "Cooking"];
+
+// const activeHobby = ["Hiking", ...hobbies];
+
+// // push all the elements
+// const person = {
+//   name: "Max",
+//   age: 30,
+// };
+
+// const copiesPerson = { address: "dublin", blood_group: "+0ve", ...person };
+// console.log(copiesPerson);
+
+// // this might be generics
+
+// // // const add = (...numbers: number[]) => {
+// //   return numbers.reduce((currResult, currValue) => {
+// //     return currResult + currValue;
+// //   }, 0);
+// // };
+
+// // same thing in tuple way
+// const add = (...numbers: [number, number, number]) => {
+//   return numbers.reduce((currResult, currValue) => {
+//     return currResult + currValue;
+//   }, 0);
+// };
+// const addNum = add(5, 6, 7);
+// console.log(addNum);
+
+// // array and object destructuring
+
+// const hobbies = ["Sport", "Cooking", "speaking"];
+// const [hobby1, hobby2, ...remaining] = hobbies; // array destructing
+
+// const person = {
+//   fname: "Max",
+//   age: 30,
+// };
+
+// const { fname, age } = person;
+
+// console.log(fname);
+
+// // class and object oriented
+
+class Department {
+  // public name: string;
+  private employees: string[] = [];
+  constructor(private readonly id: string, public name: string) {
+    // this.name = n;
+  }
+  describe(this: Department) {
+    console.log(`Department: (${this.id}): ${this.name}`);
+  }
+  addEmployee(employees: string) {
+    this.employees.push(employees);
+  }
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
-button?.addEventListener("click", clickHandler.bind(null, "me"));
 
-// next gen javascript
+const acconting = new Department("1", "Accounting");
 
-// const add = (a: number, b: number = 2) => a + b;
+acconting.describe();
 
-// console.log(add(1));
+// const accountingCopy = { name: "Dummy", describe: acconting.describe };
 
-// spread operator
+// accountingCopy.describe();
 
-const hobbies = ["Sport", "Cooking"];
+acconting.addEmployee("Max");
+acconting.addEmployee("Manu");
+acconting.addEmployee("Manaswi");
 
-const activeHobby = ["Hiking", ...hobbies];
+// avoid to write cleaner code
 
-// push all the elements
-const person = {
-  name: "Max",
-  age: 30,
-};
+// accounting.employees[2] = "anna";
 
-const copiesPerson = { address: "dublin", blood_group: "+0ve", ...person };
-console.log(copiesPerson);
-
-const add = (...numbers: number[]) => {
-  return numbers.reduce((currResult, currValue) => {
-    return currResult + currValue;
-  }, 0);
-};
-
-const addNum = add(5, 6, 7);
-console.log(addNum);
+acconting.printEmployeeInformation();
