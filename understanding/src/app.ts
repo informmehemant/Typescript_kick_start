ee84a52391e5434a21e7bdc17ae91a7b7dc00cb7
RACIT <racit@sbc_non_bank_dmz.sbic.co.za>
2024-03-03 16:00:55 +0530

    readonly modifier added

--- understanding/src/app.ts
+++ understanding/src/app.ts
@@ -1,15 +1,42 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable{
+  name: string;
+  age =  30;
+  constructor(n: string) {
+    this.name = n;
+  }
+  greet(phrase: string): void {
+    console.log(phrase +' '+ this.name)
+  }
+
+}
+
+let user1: Greetable;
+
+user1 = new Person('Max');
+
+user1.greet("Hi there - I am ");
 
 user1 = {
   name: "Max",
-  age: 35,
   greet(phrase: String) {
     console.log(phrase + '' + this.name);
   },
 };
+
+
+// readonly only modifier in interface
+
+interface Greetable1 {
+  readonly name: string;
+  greet(phrase: string):void ;
+} 
+
+type Greetable4 = {
+  readonly name: string;
+  greet(phrase: string): void ;
+}
